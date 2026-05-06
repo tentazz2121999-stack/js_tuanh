8d863c0c7b791476a33eeeaeb59f5f5f236d2ac0
tentazz2121999-stack <tentazz2121999@gmail.com>
2026-05-06 19:43:51 +0700

    udpate scripts.js

--- bai6/scripts.js
+++ bai6/scripts.js
@@ -1,17 +1,37 @@
-var a = 1;
-var b = 0;
-var pheptinh = 'chia';
-if (pheptinh === 'cong') {
-    var result = a + b;
-} else if (pheptinh === 'tru') {
-    var result = a - b;
-} else if (pheptinh === 'nhan') {
-    var result = a * b;
-} else if (pheptinh === 'chia') {
-    if(b === 0) {
-        console.log("Không thể chia cho 0");
-    } else {
-        var result = a / b;
-    }
+//vòng lặp for
+// var n = 5;
+// var sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     sum += i;
+// }
+// console.log(sum);
+
+//vòng lặp while
+var n = 5;
+var sum = 0;
+var i = 1;
+while (i <= n) {
+    sum += i;
+    i++;
 }
-console.log(result);
+console.log(sum);
+
+
+//tính toán
+// var a = 1;
+// var b = 0;
+// var pheptinh = 'chia';
+// if (pheptinh === 'cong') {
+//     var result = a + b;
+// } else if (pheptinh === 'tru') {
+//     var result = a - b;
+// } else if (pheptinh === 'nhan') {
+//     var result = a * b;
+// } else if (pheptinh === 'chia') {
+//     if(b === 0) {
+//         console.log("Không thể chia cho 0");
+//     } else {
+//         var result = a / b;
+//     }
+// }
+// console.log(result);
